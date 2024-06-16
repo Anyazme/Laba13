f014fc85303de19cf8c04cb54464211b8a4a648e
Aniiz <144984560+Anyazme@users.noreply.github.com>
2024-06-17 01:57:03 +0500

    Laba12_4

---
+++ Laba12_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using Bib10;
+namespace Laba12_4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MyCollection<Aircraft> c1 = new MyCollection<Aircraft>(10);
+			foreach (Aircraft a in c1)
+			{
+				Console.WriteLine(a);
+			}
+			Console.WriteLine();
+		}
+	}
+}
